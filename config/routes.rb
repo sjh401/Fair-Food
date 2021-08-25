d70e693f1827a26fc63b0ce304cf048880787669
stephenharrity <sjh401@gmail.com>
2021-08-25 10:48:50 -0500

    route cleanup

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   resources :comments
   resources :foods
-  resources :locations
-  resources :users
+  resources :locations, only: [:index, :show]
+  resources :users, only: :create
+  post '/auth/login', to: 'authentication#login'
+  get '/auth/verify', to: 'authentication#verify'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
