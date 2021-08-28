92903b079d49b1dd86817ec4ff402fc533cff8e7
stephenharrity <sjh401@gmail.com>
2021-08-27 20:05:00 -0500

    deleted a space

--- client/src/screens/user/Login.jsx
+++ client/src/screens/user/Login.jsx
@@ -94,7 +94,7 @@ export default function Login( {handleLogin} ) {
                             >
                                 Submit
                             </Button>
-                        </div>
+                    </div>
                 </form>
             </div>
         </div>
