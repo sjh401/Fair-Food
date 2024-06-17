5288ae3686b2ba234ce8458f59b1dd96af0e91f2
stephenharrity <sjh401@gmail.com>
2024-06-17 14:16:34 -0500

    updating

--- client/src/assets/functions.js
+++ client/src/assets/functions.js
@@ -1,14 +1,14 @@
 export const sortArray = (array) => {
-    const sorted = array.sort((a,b) => Date.parse(b.updated_at) - Date.parse(a.updated_at))
-    return sorted
+    const sorted = array.length ? array.sort((a,b) => Date.parse(b.updated_at) - Date.parse(a.updated_at)): [];
+    return sorted;
 }
 
 export const getTenIndecies = (array) => {
-    const limited = array.slice(0,10)
-    return limited
+    const limited = array.length ? array.slice(0,10) : [];
+    return limited;
 }
 
 export const filterFoods = (array, cuisine) => {
-    const filtered = array.filter(food => food.cuisine === cuisine)
-    return filtered
+    const filtered = array.length ? array.filter(food => food.cuisine === cuisine) : [];
+    return filtered;
 }
